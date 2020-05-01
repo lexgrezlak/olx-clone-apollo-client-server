c0d586609da5d537022a54825702b6335b4594a4
vnqx <accs.x@pm.me>
2020-05-01 09:42:16 +0200

    Expand NewPosting form

--- client/src/components/NewPosting.tsx
+++ client/src/components/NewPosting.tsx
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useField } from '../hooks/index';
 
-export const NewPosting = () => {
-  return <div>form</div>;
+interface FormValues {
+  title: string;
+  category: string | null;
+  description: string;
+  price: number | null;
+}
+
+interface Errors {
+  title?: string;
+  category?: string;
+  description?: string;
+  price?: string;
+}
+
+export const NewPosting: React.FC<{}> = () => {
+  const title = useField('string');
+  const category = useField('number');
+  const description = useField('string');
+  const price = useState('number');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('form sent');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Title: </label>
+        <input name="title" {...title} />
+      </div>
+      <div>
+        <label>Category: </label>
+        <input name="title" {...title} />
+      </div>
+      <div>
+        <label>Description: </label>
+        <input name="title" {...title} />
+      </div>
+      <div>
+        <label>Price: </label>
+        <input name="title" {...title} />
+      </div>
+      <div>
+        <button type="submit">Add new posting</button>
+      </div>
+    </form>
+  );
 };
---
+++ client/src/hooks/index.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export const useField = (type: string) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return { type, value, onChange };
+};
