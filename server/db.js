e59a76294d75703474ca32429ad5c43b6932d7ef
vnqx <accs.x@pm.me>
2020-05-01 05:59:50 +0200

    Initial commit

---
+++ server/db.js
@@ -0,0 +1,11 @@
+module.exports = {
+  postings: [
+    { id: 1, title: 'Suzuki GSX-F Katana', views: 254, user_id: 123 },
+    { id: 2, title: 'HP Pavillion DV9700', views: 65, user_id: 456 },
+    { id: 3, title: 'Buty Nike', views: 323, user_id: 456 },
+  ],
+  users: [
+    { id: 123, name: 'John Doe' },
+    { id: 456, name: 'Jane Doe' },
+  ],
+};
