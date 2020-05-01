771f2e8275755a8ae275ae274eaff3171b01025d
vnqx <accs.x@pm.me>
2020-05-02 01:19:35 +0200

    Fix querying

--- client/src/components/Postings.tsx
+++ client/src/components/Postings.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { Posting } from './Posting';
-import { ALL_POSTINGS } from '../graphql/queries';
 import { Item } from '../common/types';
+import { GET_POSTINGS } from '../graphql/queries';
+import { useField } from '../hooks/index';
+import Search from './Search';
 
-export const Postings: React.FC = () => {
-  const postingsQuery = useQuery(ALL_POSTINGS);
+interface Props {}
 
-  if (postingsQuery.loading) {
-    return <div>loading</div>;
-  }
+const Postings: React.FC<Props> = () => {
+  const filter = useField('text');
+  const { data, loading, error } = useQuery(GET_POSTINGS, {
+    variables: { title: filter.value },
+  });
 
-  const items = postingsQuery.data.allPostings;
+  console.log(filter.value);
 
   return (
     <div>
-      <ul>
-        {items.map((item: Item) => (
-          <Posting key={item.id} item={item} />
-        ))}
-      </ul>
+      <Search filter={filter} />
+      {!loading && !error && (
+        <ul>
+          {data.postings.map((item: Item) => (
+            <Posting key={item.id} item={item} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
+
+export default Postings;
