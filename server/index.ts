2f6eb6b9831ec5f455a681758faa7d51c3fb6c3c
vnqx <accs.x@pm.me>
2020-05-01 06:05:07 +0200

    Remove json-server

---
+++ server/index.ts
@@ -0,0 +1,5 @@
+let postings = [
+  { id: 1, title: 'Suzuki GSX-F Katana', views: 254, user_id: 123 },
+  { id: 2, title: 'HP Pavillion DV9700', views: 65, user_id: 456 },
+  { id: 3, title: 'Buty Nike', views: 323, user_id: 456 },
+];
