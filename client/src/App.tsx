be6f3f407d2992bf2548b906483b889c0250c00b
vnqx <accs.x@pm.me>
2020-05-02 01:18:22 +0200

    Fix typo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
-import { Postings } from './components/Postings';
+import Postings from './components/Postings';
 import { NewPosting } from './components/NewPosting';
+import Search from './components/Search';
+import { GET_POSTINGS } from './graphql/queries';
+import { useQuery } from '@apollo/client';
+import { Item } from './common/types';
 
 const App: React.FC = () => {
   return (
@@ -9,10 +13,10 @@ const App: React.FC = () => {
       <nav>
         <Link to="/">Home</Link>
         <Link to="/new-posting">New Posting</Link>
-        <Link to="/konto">Account</Link>
+        <Link to="/account">Account</Link>
         <Link to="/messages">Messages</Link>
         <Link to="/followed">Followed</Link>
-        <Link to="/filtered">Filtered</Link>
+        <Link to="/filtered">Filters</Link>
       </nav>
       <Switch>
         <Route path="/new-posting">
