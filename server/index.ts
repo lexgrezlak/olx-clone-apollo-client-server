4a2b17306b0490c39aabc9edc5a531bf3e044fcc
vnqx <accs.x@pm.me>
2020-05-03 08:06:27 +0200

    Add server auth

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,8 @@
 import { MONGODB_URI, JWT_SECRET } from './utils/config';
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
 import mongoose from 'mongoose';
-import User from './models/User';
 import * as jwt from 'jsonwebtoken';
 
 mongoose.set('useFindAndModify', false);
@@ -21,21 +20,27 @@ mongoose
     console.log('connecting to MongoDB failed:', error.message),
   );
 
+const getUser = (token: string) => {
+  try {
+    if (token) {
+      return jwt.verify(token, JWT_SECRET);
+    }
+
+    return null;
+  } catch (error) {
+    return null;
+  }
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null;
-    if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      const decodedToken: any = jwt.verify(auth.substring(7), JWT_SECRET);
-      console.log(decodedToken);
-      const currentUser = await User.findById(decodedToken.id).populate(
-        'postings',
-      );
-      return { currentUser };
-    }
+    const tokenWithBearer = req.headers.authorization || '';
+    const token = tokenWithBearer.split(' ')[1];
+    const user = getUser(token);
 
-    return null;
+    return { user };
   },
 });
 
