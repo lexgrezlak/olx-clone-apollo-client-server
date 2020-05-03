463869f28e6913688d241db0f8d11084b57f9894
vnqx <accs.x@pm.me>
2020-05-03 08:09:14 +0200

    Modify a query to match response

--- client/src/graphql/queries.ts
+++ client/src/graphql/queries.ts
@@ -3,7 +3,11 @@ import { gql } from '@apollo/client';
 export const LOGIN = gql`
   mutation Login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
-      value
+      token
+      user {
+        id
+        username
+      }
     }
   }
 `;
