7e4fcabe5f23d781013f12fcb528825068c98af1
vnqx <accs.x@pm.me>
2020-05-03 03:20:06 +0200

    Fix file type

---
+++ server/models/User.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  postings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Posting',
+    },
+  ],
+});
+
+userSchema.plugin(uniqueValidator);
+
+export default mongoose.model('User', userSchema);
