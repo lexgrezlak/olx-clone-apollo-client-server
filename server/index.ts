2e4f9731672fa3019cc674823e81cf2179d3b5a4
vnqx <accs.x@pm.me>
2020-05-03 02:28:57 +0200

    Connect the server to MongoDB

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,22 @@
+import { MONGODB_URI } from './utils/config';
 import { ApolloServer, gql } from 'apollo-server';
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
+import mongoose from 'mongoose';
+
+mongoose.set('useFindAndModify', false);
+
+console.log('connecting to', MONGODB_URI);
+
+mongoose
+  .connect(`${MONGODB_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error: any) =>
+    console.log('connecting to MongoDB failed:', error.message),
+  );
 
 const server = new ApolloServer({
   typeDefs,
