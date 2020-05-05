7a9267b8587ff923dc2f7ef59971e745a435160d
vnqx <accs.x@pm.me>
2020-05-05 12:42:04 +0200

    Change single quotes to double quotes

--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,23 @@
-import { MONGODB_URI, JWT_SECRET } from './utils/config';
-import { ApolloServer } from 'apollo-server';
-import typeDefs from './graphql/typeDefs';
-import resolvers from './graphql/resolvers';
-import mongoose from 'mongoose';
-import * as jwt from 'jsonwebtoken';
+import { MONGODB_URI, JWT_SECRET } from "./utils/config";
+import { ApolloServer } from "apollo-server";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
+import mongoose from "mongoose";
+import * as jwt from "jsonwebtoken";
 
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
-console.log('connecting to', MONGODB_URI);
+console.log("connecting to", MONGODB_URI);
 
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('connected to MongoDB'))
+  .then(() => console.log("connected to MongoDB"))
   .catch((error: any) =>
-    console.log('connecting to MongoDB failed:', error.message),
+    console.log("connecting to MongoDB failed:", error.message),
   );
 
 const getUser = (token: string) => {
@@ -36,8 +36,8 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
-    const tokenWithBearer = req.headers.authorization || '';
-    const token = tokenWithBearer.split(' ')[1];
+    const tokenWithBearer = req.headers.authorization || "";
+    const token = tokenWithBearer.split(" ")[1] || "";
     const user = getUser(token);
 
     return { user };
