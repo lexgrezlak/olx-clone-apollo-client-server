e472cb81e1e2ed887613f7b31619b5e92b02eeb5
vnqx <accs.x@pm.me>
2020-05-05 12:40:59 +0200

    Change user logic to isLoggedIn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,71 +1,67 @@
 import React, { useState, useEffect } from "react";
-import { Link, Route, Switch, useHistory } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Postings from "./components/Postings";
 import NewPosting from "./components/NewPosting";
-import { useApolloClient } from "@apollo/client";
+import { useQuery, useApolloClient } from "@apollo/client";
 import AccountDashboard from "./components/AccountDashboard";
 import Filters from "./components/Filters";
 import AccountMessages from "./components/AccountMessages";
 import AccountFollowed from "./components/AccountFollowed";
 import Login from "./components/Login";
-import { User } from "./common/types";
 import Navigation from "./components/Navigation";
+import { GET_CURRENT_USER } from "./graphql/queries";
 
 const App: React.FC = () => {
   const [needToRefetch, setNeedToRefetch] = useState(false);
-  const [user, setUser] = useState<User | null>(null);
-  const [error, setError] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const client = useApolloClient();
-  const history = useHistory();
+  const { data } = useQuery(GET_CURRENT_USER, {
+    onError: (error) => {
+      console.log(error.graphQLErrors[0].message, "dpdd");
+    },
+  });
 
   useEffect(() => {
-    const tokenAndUserJSON = window.localStorage.getItem("olx-clone-user");
-    if (tokenAndUserJSON) {
-      const tokenAndUser = JSON.parse(tokenAndUserJSON);
-      setUser(tokenAndUser.user);
+    if (data && data.currentUser) {
+      setIsLoggedIn(true);
     }
-  }, []);
+  }, [data, setIsLoggedIn]);
 
   const handleLogout = (event: React.MouseEvent) => {
-    event.preventDefault();
-    setUser(null);
-    window.localStorage.clear();
-    client.resetStore();
-    history.push("/");
+    localStorage.clear();
+    setIsLoggedIn(false);
   };
 
   return (
     <div>
       <header>
         <Navigation />
-        {/* <nav>
-          <Link to="/">Home</Link>
-          <Link to="/new-posting">New Posting</Link>
-          <Link to="/account">Account</Link>
-          <Link to="/account/messages">Messages</Link>
-          <Link to="/account/followed">Followed</Link>
-          <Link to="/filtered">Filters</Link>
-        </nav> */}
       </header>
       <main>
         <Switch>
           <Route path="/new-posting">
-            <NewPosting setNeedToRefetch={setNeedToRefetch} user={user} />
+            <NewPosting
+              setNeedToRefetch={setNeedToRefetch}
+              isLoggedIn={isLoggedIn}
+            />
           </Route>
           <Route path="/account/messages">
-            <AccountMessages user={user} />
+            <AccountMessages isLoggedIn={isLoggedIn} />
           </Route>
           <Route path="/account/followed">
-            <AccountFollowed user={user} />
+            <AccountFollowed isLoggedIn={isLoggedIn} />
           </Route>
           <Route path="/account">
-            <AccountDashboard user={user} handleLogout={handleLogout} />
+            <AccountDashboard
+              handleLogout={handleLogout}
+              isLoggedIn={isLoggedIn}
+            />
           </Route>
           <Route path="/filters">
             <Filters />
           </Route>
           <Route path="/login">
-            <Login setUser={setUser} setError={setError} />
+            <Login setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
           </Route>
           <Route path="/">
             <Postings
