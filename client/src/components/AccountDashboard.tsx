8453b233c0020e15922add505f3abc4187c64695
vnqx <accs.x@pm.me>
2020-05-06 15:20:41 +0200

    Add proper basic dashboard

--- client/src/components/AccountDashboard.tsx
+++ client/src/components/AccountDashboard.tsx
@@ -1,24 +1,99 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  createStyles,
+  IconButton,
+  useTheme,
+} from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import { Delete } from "@material-ui/icons";
+import { useApolloClient } from "@apollo/client";
 
-interface Props {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    details: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    content: {
+      flex: "1 0 auto",
+    },
+    cover: {
+      width: 151,
+    },
+    controls: {
+      display: "flex",
+      alignItems: "center",
+      paddingLeft: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
+    },
+    deleteIcon: {
+      height: 38,
+      width: 38,
+    },
+  })
+);
+
+export default function AccountDashboard({
+  user,
+  handleLogout,
+}: {
+  user: any;
   handleLogout: any;
-  isLoggedIn: boolean;
-}
+}) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const client = useApolloClient();
+  const history = useHistory();
 
-const AccountDashboard: React.FC<Props> = ({ isLoggedIn, handleLogout }) => {
-  if (!isLoggedIn) {
+  if (user === null) {
     return <Redirect push to="/login" />;
   }
 
+  const { postings } = user;
+
   return (
     <div>
-      hello man
-      <button type="button" onClick={handleLogout}>
-        logout
-      </button>
+      <div>
+        <Typography component="h4" variant="h4">
+          Welcome {user.email}
+        </Typography>
+        <Button onClick={handleLogout} variant="outlined">
+          logout
+        </Button>
+      </div>
+      {postings.map((posting: any) => (
+        <Card key={posting.id} className={classes.root}>
+          <CardMedia
+            className={classes.cover}
+            title={posting.title}
+            image={posting.imageUrls[0]}
+          />
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography variant="h5" component="h5">
+                {posting.title}
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary">
+                ${posting.price}
+              </Typography>
+            </CardContent>
+            <div className={classes.controls}>
+              <IconButton aria-label="delete">
+                <Delete className={classes.deleteIcon} />
+              </IconButton>
+            </div>
+          </div>
+        </Card>
+      ))}
     </div>
   );
-};
-
-export default AccountDashboard;
+}
