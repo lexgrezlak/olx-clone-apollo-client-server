0b6d0faacd995abd9f5a04dc67047472f6afe080
vnqx <accs.x@pm.me>
2020-05-07 17:34:26 +0200

    Delete authentication error and extend type User

--- server/users/index.ts
+++ server/users/index.ts
@@ -1,31 +1,24 @@
 import { Parent } from "../types";
 import { IContext } from "../context";
-import { gql, AuthenticationError } from "apollo-server";
+import { gql } from "apollo-server";
 import { IUserIdArgs } from "./types";
 
 export const userTypeDefs = gql`
   type User {
     id: ID!
     email: String!
+    name: String
+    postings: [Posting!]!
   }
 
   extend type Query {
-    currentUser: User!
+    currentUser: User
   }
 `;
 
 export const userResolvers = {
   Query: {
-    currentUser: async (
-      _parent: Parent,
-      _args: IUserIdArgs,
-      { user }: IContext
-    ) => {
-      if (!user) throw new AuthenticationError("Not authenticated");
-      // //@ts-ignore
-      // const userInDb = await User.findById(user.id);
-      // return userInDb;
-      return user;
-    },
+    currentUser: (_parent: Parent, _args: IUserIdArgs, { user }: IContext) =>
+      user,
   },
 };
