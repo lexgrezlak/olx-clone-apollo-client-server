07bf26ea58bb30262ea53135e6d7c0f13095dd09
vnqx <accs.x@pm.me>
2020-05-07 14:53:57 +0200

    Move out Copyright and rename the component

--- client/src/components/SignInPage.tsx
+++ client/src/components/SignInPage.tsx
@@ -1,37 +1,3 @@
-// import React, { useEffect } from "react";
-// import { Typography, TextField, Button } from "@material-ui/core";
-// import { useField } from "../hooks/index";
-// import { LOGIN } from "../graphql/queries";
-// import { useMutation } from "@apollo/client";
-// import { useHistory } from "react-router-dom";
-//
-// interface Props {
-//   setIsLoggedIn: Function;
-//   isLoggedIn: boolean;
-// }
-//
-// const Login: React.FC<Props> = ({ setIsLoggedIn, isLoggedIn }) => {
-//   const email = useField("email");
-//   const password = useField("password");
-//   const history = useHistory();
-//
-//   const [login, { data }] = useMutation(LOGIN, {
-//     onError: (error) => {
-//       console.log(error.graphQLErrors[0].message);
-//     },
-//   });
-//
-//   useEffect(() => {
-//     if (data && data.login) {
-//       const { token } = data.login;
-//       localStorage.setItem("token", token);
-//       setIsLoggedIn(true);
-//     }
-//   }, [data, setIsLoggedIn]);
-//
-//   if (isLoggedIn) history.goBack();
-//
-
 import React, { useEffect } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
@@ -41,27 +7,14 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { useField } from "../hooks";
 import { useApolloClient, useMutation } from "@apollo/client";
+import { useField } from "../hooks";
+import { SIGN_IN } from "../graphql/queries";
 import { useHistory } from "react-router-dom";
-import { LOGIN } from "../graphql/queries";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://localhost:3000/">
-        Clolx
-      </Link>{" "}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -83,33 +36,33 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Login({ user }: { user: any }) {
+export default function SignInPage({ setIsLoggedIn }: any) {
   const classes = useStyles();
   const email = useField("email");
   const password = useField("password");
-  const history = useHistory();
   const client = useApolloClient();
+  const history = useHistory();
 
-  const [login, { data }] = useMutation(LOGIN, {
+  const [signIn, { data }] = useMutation(SIGN_IN, {
     onError: (error) => {
       console.log(error.graphQLErrors[0].message);
     },
   });
 
   useEffect(() => {
-    if (data && data.login) {
-      const { token } = data.login;
+    if (data && data.signIn) {
+      const { token } = data.signIn;
       localStorage.setItem("token", token);
+      setIsLoggedIn(true);
       client.resetStore();
+      history.goBack();
     }
-  }, [data]);
-
-  if (user !== null) history.goBack();
+  }, [client, data, setIsLoggedIn]);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    login({
-      variables: { email: email.value, password: password.value },
+    signIn({
+      variables: { input: { email: email.value, password: password.value } },
     });
   };
 
@@ -162,19 +115,18 @@ export default function Login({ user }: { user: any }) {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link href="/forgotpassword" variant="body2">
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link variant="body2">Don't have an account? Sign Up</Link>
+              <Link href="/signup" variant="body2">
+                Don&apos;t have an account? Sign Up
+              </Link>
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
     </Container>
   );
 }
