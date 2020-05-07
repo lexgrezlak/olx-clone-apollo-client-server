45f1c73be5bc812af93445aa1ed94c4fd60ffb47
vnqx <accs.x@pm.me>
2020-05-07 16:03:13 +0200

    Add missing dependency

--- client/src/components/SignInPage.tsx
+++ client/src/components/SignInPage.tsx
@@ -3,8 +3,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
@@ -57,7 +55,7 @@ export default function SignInPage({ setIsLoggedIn }: any) {
       client.resetStore();
       history.goBack();
     }
-  }, [client, data, setIsLoggedIn]);
+  }, [client, data, history, setIsLoggedIn]);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -99,10 +97,6 @@ export default function SignInPage({ setIsLoggedIn }: any) {
             id="password"
             autoComplete="current-password"
             {...password}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
           />
           <Button
             type="submit"
