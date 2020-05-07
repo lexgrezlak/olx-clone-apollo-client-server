859aa50d2746b60b85dcf971086d56d82ed6c727
vnqx <accs.x@pm.me>
2020-05-07 14:48:47 +0200

    Move out redirect

--- client/src/components/AccountFollowed.tsx
+++ client/src/components/AccountFollowed.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
 
-interface Props {
-  user: any;
-}
-
-const AccountFollowed: React.FC<Props> = ({ user }) => {
-  if (user === null) {
-    return <Redirect push to="/login" />;
-  }
-
+function AccountFollowed() {
   return <div>followed</div>;
-};
+}
 
 export default AccountFollowed;
