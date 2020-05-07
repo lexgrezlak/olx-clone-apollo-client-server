19fcc41c1d8117efde7e2eb3f97684e16fb97fc3
vnqx <accs.x@pm.me>
2020-05-07 14:48:50 +0200

    Move out redirect

--- client/src/components/AccountMessages.tsx
+++ client/src/components/AccountMessages.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
 
-interface Props {
-  user: any;
-}
-
-const AccountMessages: React.FC<Props> = ({ user }) => {
-  if (user === null) {
-    return <Redirect push to="/login" />;
-  }
-
+function AccountMessages() {
   return <div>hello messages </div>;
-};
+}
 
 export default AccountMessages;
