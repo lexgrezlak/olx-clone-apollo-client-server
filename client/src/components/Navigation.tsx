f97bd4fe4b3e2154ed87a7a0480f6fc5ae492684
vnqx <accs.x@pm.me>
2020-05-09 17:18:14 +0200

    Rename the component

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -26,7 +26,7 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export default function MenuAppBar() {
+export default function Navigation() {
   const classes = useStyles();
 
   return (
