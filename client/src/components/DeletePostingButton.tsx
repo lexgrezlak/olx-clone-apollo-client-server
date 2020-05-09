94be373f793ab121be97e85cf193aeb70a6d26a5
vnqx <accs.x@pm.me>
2020-05-09 13:30:16 +0200

    Add DeletePostingButton

---
+++ client/src/components/DeletePostingButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useApolloClient, useMutation } from "@apollo/client";
+import { DELETE_POSTING } from "../graphql/queries";
+import { IconButton } from "@material-ui/core";
+import { Delete } from "@material-ui/icons";
+
+function DeletePostingButton({ postingId, css }: any) {
+  const client = useApolloClient();
+
+  const [deletePosting] = useMutation(DELETE_POSTING, {
+    onError: (error) => {
+      console.log(error.graphQLErrors[0].message);
+    },
+  });
+
+  async function handleDelete(id: string) {
+    await deletePosting({ variables: { id } });
+    await client.resetStore();
+  }
+
+  return (
+    <IconButton aria-label="delete" onClick={() => handleDelete(postingId)}>
+      <Delete className={css} />
+    </IconButton>
+  );
+}
+
+export default DeletePostingButton;
