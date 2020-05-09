b9849d36b07fb84b64b66d1813c850926a4d8865
vnqx <accs.x@pm.me>
2020-05-09 17:18:39 +0200

    Query for postings by title instead of all postings

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 import { useField } from "../hooks";
-import {
-  GET_ALL_POSTINGS,
-  GET_CURRENT_USER_FOLLOWED_POSTINGS_IDS,
-} from "../graphql/queries";
+import { GET_ALL_POSTINGS, GET_POSTINGS_BY_TITLE } from "../graphql/queries";
 import Search from "../components/Search";
 import Postings from "../components/Postings";
 
@@ -25,15 +22,16 @@ export default function Home() {
   const filter = useField("search");
   const classes = useStyles();
   const [postings, setPostings] = useState([]);
-  const { data: postingsData } = useQuery(GET_ALL_POSTINGS, {
+  const { data: postingsData } = useQuery(GET_POSTINGS_BY_TITLE, {
+    variables: { title: filter.value },
     onError: (error) => {
       console.log(error.graphQLErrors[0].message);
     },
   });
 
   useEffect(() => {
-    if (postingsData?.allPostings) {
-      setPostings(postingsData.allPostings);
+    if (postingsData?.postingsByTitle) {
+      setPostings(postingsData.postingsByTitle);
     }
   }, [postingsData]);
 
