8422c7f21b54acbe22ab5e20d2da44d491157f11
vnqx <accs.x@pm.me>
2020-05-09 09:07:28 +0200

    Delete testUtils

--- server/utils/testUtils.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloServer } from "apollo-server";
-import typeDefs from "../typeDefs";
-import resolvers from "../resolvers";
-
-export const createTestServer = (context: any) =>
-  new ApolloServer({ typeDefs, resolvers, context });
