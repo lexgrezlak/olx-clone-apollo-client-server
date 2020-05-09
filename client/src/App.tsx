a469d65e6bf883da997614606ffadb3eada2133e
vnqx <accs.x@pm.me>
2020-05-09 09:03:04 +0200

    Add full posting route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useRouteMatch } from "react-router-dom";
 import { useApolloClient, useQuery } from "@apollo/client";
 import Box from "@material-ui/core/Box";
-import Postings from "./components/Postings";
-import NewPosting from "./components/NewPosting";
+import AllPostings from "./pages/AllPostings";
+import NewPosting from "./pages/NewPosting";
 import AccountDashboard from "./components/AccountDashboard";
 import Filters from "./components/Filters";
 import AccountMessages from "./components/AccountMessages";
 import AccountFollowed from "./components/AccountFollowed";
-import SignInPage from "./components/SignInPage";
+import SignIn from "./pages/SignIn";
 import Navigation from "./components/Navigation";
 import Copyright from "./components/Copyright";
-import SignUpPage from "./components/SignUpPage";
+import SignUp from "./pages/SignUp";
 import { GET_CURRENT_USER } from "./graphql/queries";
+import FullPosting from "./pages/FullPosting";
 
-const App: React.FC = () => {
+export default function App() {
   const client = useApolloClient();
   const [user, setUser] = useState(null);
-  const { data, loading } = useQuery(GET_CURRENT_USER);
+  const [followedPostings, setFollowedPostings] = useState([]);
+  const { data, loading, error } = useQuery(GET_CURRENT_USER);
+  const match = useRouteMatch("/posting/:id") as any;
 
   useEffect(() => {
     if (data && data.currentUser) {
-      setUser(data.currentUser);
+      const { currentUser } = data;
+      setUser(currentUser);
+      setFollowedPostings(currentUser.followedPostings);
     }
   }, [client, data]);
 
   if (loading) return null;
+  if (error) return <div>error</div>;
 
   return (
     <div>
@@ -37,11 +43,18 @@ const App: React.FC = () => {
           <Route path="/newposting">
             {user ? <NewPosting /> : <Redirect push to="/signin" />}
           </Route>
+          <Route path="/posting/:id">
+            <FullPosting id={match ? match.params.id : null} />
+          </Route>
           <Route path="/account/messages">
             {user ? <AccountMessages /> : <Redirect push to="/signin" />}
           </Route>
           <Route path="/account/followed">
-            {user ? <AccountFollowed /> : <Redirect push to="/signin" />}
+            {user ? (
+              <AccountFollowed followedPostings={followedPostings} />
+            ) : (
+              <Redirect push to="/signin" />
+            )}
           </Route>
           <Route path="/account">
             {user ? (
@@ -54,13 +67,16 @@ const App: React.FC = () => {
             <Filters />
           </Route>
           <Route path="/signin">
-            {!user ? <SignInPage /> : <Redirect to="/" />}
+            {!user ? <SignIn /> : <Redirect to="/" />}
           </Route>
           <Route path="/signup">
-            {!user ? <SignUpPage /> : <Redirect to="/" />}
+            {!user ? <SignUp /> : <Redirect to="/" />}
           </Route>
           <Route exact path="/">
-            <Postings />
+            <AllPostings
+              followedPostings={followedPostings}
+              setFollowedPostings={setFollowedPostings}
+            />
           </Route>
           <Route path="/">
             <div>Page not found</div>
@@ -74,6 +90,4 @@ const App: React.FC = () => {
       </footer>
     </div>
   );
-};
-
-export default App;
+}
