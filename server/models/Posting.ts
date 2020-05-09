b13b1dbf1e60a493c0a0b2445f772f078ba8a9fc
vnqx <accs.x@pm.me>
2020-05-09 13:33:18 +0200

    Enable timestamps

--- server/models/Posting.ts
+++ server/models/Posting.ts
@@ -13,51 +13,54 @@ export interface IPosting extends Document {
   user: IUser["_id"];
 }
 
-const PostingSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 20,
+const PostingSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 200,
+    },
+    imageUrls: {
+      type: [String],
+      required: true,
+      minlength: 1,
+      maxlength: 8,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    condition: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      maxlength: 20,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      maxlength: 20,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  category: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    maxlength: 200,
-  },
-  imageUrls: {
-    type: [String],
-    required: true,
-    minlength: 1,
-    maxlength: 8,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  condition: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-    maxlength: 20,
-  },
-  phone: {
-    type: Number,
-    required: true,
-    maxlength: 20,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 PostingSchema.set("toJSON", {
   transform: (_document, returnedObject) => {
