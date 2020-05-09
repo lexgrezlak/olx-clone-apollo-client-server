1a43a111b66602f4187f811b9311a1f30e395afc
vnqx <accs.x@pm.me>
2020-05-09 13:31:40 +0200

    Add LastUpdated component

---
+++ client/src/components/LastUpdated.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+function LastUpdated({ updatedAt }: any) {
+  const updatedAtDate = new Date(updatedAt);
+  const now = new Date();
+
+  const msSinceUpdated = now.getTime() - updatedAtDate.getTime();
+  const daysSinceUpdated = Math.floor(msSinceUpdated / (1000 * 60 * 60 * 24));
+
+  return (
+    <Typography variant="subtitle1" color="textSecondary">
+      Last updated: {/* eslint-disable-next-line no-nested-ternary */}
+      {daysSinceUpdated === 0
+        ? "today"
+        : daysSinceUpdated === 1
+        ? "yesterday"
+        : `${daysSinceUpdated} days ago`}
+    </Typography>
+  );
+}
+
+export default LastUpdated;
