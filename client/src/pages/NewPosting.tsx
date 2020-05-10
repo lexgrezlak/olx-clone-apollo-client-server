2d81fa03c3384f306f20f7a6ce6e6722eac2ac33
vnqx <accs.x@pm.me>
2020-05-10 12:58:38 +0200

    Add styling and redirect to the posting on submit

--- client/src/pages/NewPosting.tsx
+++ client/src/pages/NewPosting.tsx
@@ -9,12 +9,36 @@ import {
   MenuItem,
   FormControl,
   InputLabel,
+  createStyles,
+  Container,
+  CssBaseline,
 } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import { ADD_POSTING } from "../graphql/queries";
 import { useField } from "../hooks";
 import UploadPhotos from "../components/UploadPhotos";
 import UploadedPhotos from "../components/UploadedPhotos";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+      // marginTop: "16px",
+      // marginBottom: "8px",
+    },
+  })
+);
 
 function NewPosting() {
   const title = useField("text");
@@ -28,6 +52,7 @@ function NewPosting() {
   const condition = useField("radio");
   const history = useHistory();
   const client = useApolloClient();
+  const classes = useStyles();
 
   const CATEGORIES = ["Fashion", "Electronics", "Health"];
   const CONDITIONS = ["New", "Used"];
@@ -35,7 +60,7 @@ function NewPosting() {
   async function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
 
-    await addPosting({
+    const addedPosting = await addPosting({
       variables: {
         title: title.value,
         category: category.value,
@@ -48,65 +73,103 @@ function NewPosting() {
       },
     });
     await client.resetStore();
-    // TODO push to the posting
-    history.push("/");
+    history.push(`/posting/${addedPosting.data.addPosting.id}`);
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <Typography variant="h3" style={{ margin: "20px" }}>
-        Add new posting
-      </Typography>
-      <TextField fullWidth variant="outlined" label="Title" {...title} />
-      <FormControl fullWidth variant="outlined">
-        <InputLabel id="category-label">Category</InputLabel>
-        {
-          // @ts-ignore
-          <Select labelId="category-label" id="category" {...category}>
-            {CATEGORIES.map((thisCategory) => (
-              <MenuItem key={thisCategory} value={thisCategory}>
-                {thisCategory}
-              </MenuItem>
-            ))}
-          </Select>
-        }
-      </FormControl>
-      <TextField
-        multiline
-        fullWidth
-        variant="outlined"
-        label="Description"
-        {...description}
-      />
-      <TextField
-        fullWidth
-        variant="outlined"
-        label="Price"
-        {...price}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-        }}
-      />
-      <FormControl fullWidth variant="outlined">
-        <InputLabel id="category-label">Condition</InputLabel>
-        {
-          // @ts-ignore
-          <Select labelId="condition-label" id="condition" {...condition}>
-            {CONDITIONS.map((thisCondition) => (
-              <MenuItem key={thisCondition} value={thisCondition}>
-                {thisCondition}
-              </MenuItem>
-            ))}
-          </Select>
-        }
-      </FormControl>
-      <TextField fullWidth variant="outlined" label="Phone" {...phone} />
-      <TextField fullWidth variant="outlined" label="City" {...city} />
-      <UploadPhotos urls={urls} setUrls={setUrls} />
-      <UploadedPhotos urls={urls} setUrls={setUrls} />
-      <Button color="primary" size="large" variant="contained" type="submit">
-        Add
-      </Button>
-    </form>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Add new posting
+        </Typography>
+        <form onSubmit={handleSubmit} noValidate className={classes.form}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            variant="outlined"
+            label="Title"
+            {...title}
+          />
+          <FormControl margin="normal" fullWidth variant="outlined">
+            <InputLabel id="category-label" required>
+              Category
+            </InputLabel>
+            {
+              // @ts-ignore
+              <Select labelId="category-label" id="category" {...category}>
+                {CATEGORIES.map((CATEGORY: string) => (
+                  <MenuItem key={CATEGORY} value={CATEGORY}>
+                    {CATEGORY}
+                  </MenuItem>
+                ))}
+              </Select>
+            }
+          </FormControl>
+          <TextField
+            margin="normal"
+            rows={4}
+            multiline
+            fullWidth
+            variant="outlined"
+            label="Description"
+            {...description}
+          />
+          <TextField
+            fullWidth
+            margin="normal"
+            required
+            variant="outlined"
+            label="Price"
+            {...price}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              ),
+            }}
+          />
+          <FormControl margin="normal" fullWidth variant="outlined" required>
+            <InputLabel id="category-label">Condition</InputLabel>
+            {
+              // @ts-ignore
+              <Select labelId="condition-label" id="condition" {...condition}>
+                {CONDITIONS.map((thisCondition) => (
+                  <MenuItem key={thisCondition} value={thisCondition}>
+                    {thisCondition}
+                  </MenuItem>
+                ))}
+              </Select>
+            }
+          </FormControl>
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            label="Phone"
+            {...phone}
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            label="City"
+            {...city}
+          />
+          <UploadPhotos urls={urls} setUrls={setUrls} />
+          <UploadedPhotos urls={urls} setUrls={setUrls} />
+          <Button
+            color="primary"
+            size="large"
+            variant="contained"
+            type="submit"
+            fullWidth
+            className={classes.submit}
+          >
+            Add
+          </Button>
+        </form>
+      </div>
+    </Container>
   );
 }
 
