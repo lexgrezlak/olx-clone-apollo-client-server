cbd9a4e76108ce0958003fcc443eba4a94442eb6
vnqx <accs.x@pm.me>
2020-05-10 12:59:28 +0200

    Add PhonePopover

---
+++ client/src/components/PhonePopover.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
+import { IconButton } from "@material-ui/core";
+import PhoneIcon from "@material-ui/icons/Phone";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    typography: {
+      padding: theme.spacing(2),
+    },
+  })
+);
+
+export default function PhonePopover({ phone }: any) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "phone-popover" : undefined;
+
+  return (
+    <div>
+      <IconButton onClick={handleClick} aria-label="see the phone number">
+        <PhoneIcon />
+      </IconButton>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <Typography className={classes.typography}>{phone}</Typography>
+      </Popover>
+    </div>
+  );
+}
