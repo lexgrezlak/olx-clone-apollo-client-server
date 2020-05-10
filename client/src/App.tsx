495f055a85dc60abb90134ed0b84d4a16cf733fd
vnqx <accs.x@pm.me>
2020-05-10 12:54:34 +0200

    Add styling and move follow query into App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, Redirect, useRouteMatch } from "react-router-dom";
-import { useApolloClient, useQuery } from "@apollo/client";
-import Box from "@material-ui/core/Box";
+import { useQuery } from "@apollo/client";
+import { makeStyles } from "@material-ui/core/styles";
 import Home from "./pages/Home";
 import NewPosting from "./pages/NewPosting";
 import Dashboard from "./pages/Dashboard";
@@ -10,28 +10,60 @@ import Messages from "./pages/Messages";
 import Followed from "./pages/Followed";
 import SignIn from "./pages/SignIn";
 import Navigation from "./components/Navigation";
-import Copyright from "./components/Copyright";
 import SignUp from "./pages/SignUp";
-import { GET_IS_LOGGED_IN } from "./graphql/queries";
+import {
+  GET_CURRENT_USER_FOLLOWED_POSTINGS_IDS,
+  GET_IS_LOGGED_IN,
+} from "./graphql/queries";
 import FullPosting from "./pages/FullPosting";
 import PrivateRoute from "./components/PrivateRoute";
+import Footer from "./components/Footer";
+import EditPosting from "./pages/EditPosting";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+}));
 
 export default function App() {
-  const client = useApolloClient();
+  const classes = useStyles();
   const [isLoggedIn, setIsLoggedIn] = useState<null | boolean>(null);
-  const { data } = useQuery(GET_IS_LOGGED_IN);
-  const match = useRouteMatch("/posting/:id") as any;
+  const { data: loggedData } = useQuery(GET_IS_LOGGED_IN);
+  const fullMatch = useRouteMatch("/posting/:id") as any;
+  const editMatch = useRouteMatch("/edit/:id") as any;
+  const [followedPostingsIds, setFollowedPostingsIds] = useState<string[]>([]);
+
+  const { data: followedData } = useQuery(
+    GET_CURRENT_USER_FOLLOWED_POSTINGS_IDS,
+    {
+      onError: (error) => {
+        console.log(error.graphQLErrors[0].message);
+      },
+    }
+  );
 
   useEffect(() => {
-    if (data?.isLoggedIn) {
+    if (followedData?.currentUserFollowedPostings) {
+      const ids = followedData.currentUserFollowedPostings.map(
+        (posting: any) => posting.id
+      );
+      setFollowedPostingsIds(ids);
+    }
+  }, [followedData]);
+
+  useEffect(() => {
+    if (loggedData?.isLoggedIn) {
       setIsLoggedIn(true);
     } else {
       setIsLoggedIn(false);
     }
-  }, [client, data]);
+  }, [loggedData]);
 
   return (
-    <div>
+    <div className={classes.root}>
       <header>
         <Navigation />
       </header>
@@ -41,7 +73,10 @@ export default function App() {
             <NewPosting />
           </PrivateRoute>
           <Route path="/posting/:id">
-            <FullPosting id={match ? match.params.id : null} />
+            <FullPosting
+              id={fullMatch ? fullMatch.params.id : null}
+              followedPostingsIds={followedPostingsIds}
+            />
           </Route>
           <PrivateRoute isLoggedIn={isLoggedIn} path="/account/messages">
             <Messages />
@@ -51,6 +86,9 @@ export default function App() {
           </PrivateRoute>
           <PrivateRoute isLoggedIn={isLoggedIn} path="/account">
             <Dashboard />
+          </PrivateRoute>
+          <PrivateRoute isLoggedIn={isLoggedIn} path="/edit/:id">
+            <EditPosting id={editMatch ? editMatch.params.id : null} />
           </PrivateRoute>
           <Route path="/filters">
             <Filters />
@@ -62,18 +100,14 @@ export default function App() {
             {!isLoggedIn ? <SignUp /> : <Redirect to="/account" />}
           </Route>
           <Route exact path="/">
-            <Home />
+            <Home followedPostingsIds={followedPostingsIds} />
           </Route>
           <Route path="/">
             <div>Page not found</div>
           </Route>
         </Switch>
       </main>
-      <footer>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </footer>
+      <Footer />
     </div>
   );
 }
