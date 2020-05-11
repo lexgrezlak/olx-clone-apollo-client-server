290df85c667f0dfa3d3bd150826ac9ce0ed4cca7
vnqx <accs.x@pm.me>
2020-05-11 18:34:07 +0200

    Add message model

---
+++ server/models/Message.ts
@@ -0,0 +1,44 @@
+import { model, Schema, Document } from "mongoose";
+import { IUser } from "./User";
+import { IPosting } from "./Posting";
+
+export interface IMessage extends Document {
+  content: string;
+  posting: IPosting;
+  fromUser: IUser["_id"];
+  toUser: IUser["_id"];
+}
+
+const MessageSchema = new Schema(
+  {
+    content: {
+      type: String,
+      minlength: 1,
+      maxlength: 250,
+      required: true,
+    },
+    posting: {
+      type: Schema.Types.ObjectId,
+      ref: "Posting",
+    },
+    fromUser: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    toUser: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+MessageSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export default model<IMessage>("Message", MessageSchema);
