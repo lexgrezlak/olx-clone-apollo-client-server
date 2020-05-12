5845f5c3cb1236f8e592830b8e24d16692979514
vnqx <accs.x@pm.me>
2020-05-12 19:11:52 +0200

    Add message resolvers

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -1,12 +1,19 @@
 import { userResolvers } from "./users";
 import { postingResolvers } from "./postings";
 import { authResolvers } from "./auth";
+import { messageResolvers } from "./messages";
 
 export default {
-  Query: Object.assign({}, userResolvers.Query, postingResolvers.Query),
+  Query: Object.assign(
+    {},
+    userResolvers.Query,
+    postingResolvers.Query,
+    messageResolvers.Query
+  ),
   Mutation: Object.assign(
     {},
     authResolvers.Mutation,
-    postingResolvers.Mutation
+    postingResolvers.Mutation,
+    messageResolvers.Mutation
   ),
 };
