dff2217dba8bc046296576c5620f066e78257236
vnqx <accs.x@pm.me>
2020-05-12 19:03:47 +0200

    Refactor launch button to its own component

---
+++ client/src/components/LaunchButton.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import { Link } from "react-router-dom";
+import LaunchIcon from "@material-ui/icons/Launch";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    launch: {
+      color: "deepskyblue",
+    },
+  })
+);
+
+function LaunchButton({ id }: any) {
+  const classes = useStyles();
+
+  return (
+    <IconButton
+      className={classes.launch}
+      aria-label="open the posting"
+      component={Link}
+      to={`/posting/${id}`}
+    >
+      <LaunchIcon />
+    </IconButton>
+  );
+}
+
+export default LaunchButton;
