0b461c88527b6fc61358c3aa4096a3692af1ac6e
vnqx <accs.x@pm.me>
2020-05-12 19:12:04 +0200

    Add message typeDefs

--- server/typeDefs.ts
+++ server/typeDefs.ts
@@ -2,10 +2,17 @@ import { gql } from "apollo-server";
 import { userTypeDefs } from "./users";
 import { authTypeDefs } from "./auth";
 import { postingTypeDefs } from "./postings";
+import { messageTypeDefs } from "./messages";
 
 const baseTypeDefs = gql`
   type Query
   type Mutation
 `;
 
-export default [baseTypeDefs, userTypeDefs, authTypeDefs, postingTypeDefs];
+export default [
+  baseTypeDefs,
+  userTypeDefs,
+  authTypeDefs,
+  postingTypeDefs,
+  messageTypeDefs,
+];
