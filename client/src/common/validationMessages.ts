a436f7285e1f1fae0fb272403a8d37d95e839acd
vnqx <accs.x@pm.me>
2020-05-15 16:36:25 +0200

    Add validation messages

---
+++ client/src/common/validationMessages.ts
@@ -0,0 +1,9 @@
+export const minMessage = (number: number) =>
+  `Minimum ${number} characters allowed`;
+
+export const maxMessage = (number: number) =>
+  `Maximum ${number} characters allowed`;
+
+export const requiredMessage = "Required";
+
+export const invalidValue = (value: string) => `Invalid ${value}`;
